eee3376d04fa99117edfdacb9bb1ce81d524a007
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-03-11 12:19:19 +0100

    init

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CircleShape shape = new CircleShape(100);
+            shape.FillColor = Color.Cyan;
+            shape.Position = new SFML.System.Vector2f(150, 150);
+
+
+            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(500, 500), "HelloWorld");
+            while (window.IsOpen)
+            {
+                window.Clear();
+                window.Draw(shape);
+                window.Display();
+    
+
+            }
+        }
+    }
+}
