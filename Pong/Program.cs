3a4bd10ed9e22e7102974848c74fd75ab324594c
Jannis Jahr <jjahr@stud.hs-bremen.de>
2016-03-18 13:10:17 +0100

    Revert "test"
    This reverts commit 88bfcc7f20116d936698b43da79b1b24dd16f077.

--- Pong/Program.cs
+++ Pong/Program.cs
@@ -15,10 +15,9 @@ namespace Pong
 
         static void Main(string[] args)
         {
-            uint ResX = 500;
-            uint ResY = 500;
+            int X = 500
             Player PlayerOne = new Player();
-            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(ResX, ResY), "HelloWorld");
+            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(500, 500), "HelloWorld");
             InputHandler inputHandler = new InputHandler(window);
             Ball ball = new Ball(new Vector2f(10, 0), new Vector2f(250, 250));
             window.SetFramerateLimit(50);
