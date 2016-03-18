2fa2a7772c308fbe33c393ed026c3a285a53ab0b
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-03-18 13:14:25 +0100

    resolved problem with my balls

---
+++ Pong/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Graphics;
+
+namespace Pong
+{
+    class Ball
+    {
+        public Ball(Vector2f deltaXY, Vector2f position)
+        {
+            circle = new CircleShape(10);
+            this.deltaXY = deltaXY;
+            this.position = new Vector2f(position.X - (1 / 2 * circle.Radius), position.Y - (1 / 2 * circle.Radius));
+        }
+        public CircleShape circle;
+        Vector2f deltaXY;
+        Vector2f position;
+
+        public void updatePosition()
+        {
+            if (position.X < 1 || position.X > 499)
+            {
+                deltaXY.X = -deltaXY.X;
+            }
+            position.X = position.X + deltaXY.X;
+            circle.Position = position;
+        }
+
+    }
+
+
+
+}
