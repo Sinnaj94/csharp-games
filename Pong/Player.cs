7f2c2e96bee5b797284c5a8b0708d65958ef9a8a
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-03-18 10:51:34 +0100

    Added Playerclass

---
+++ Pong/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Pong
+{
+    class Player
+    {
+
+        public Player()
+        {
+            _PlayerOnePosition = new Vector2f(10, 100);
+            _playerOneSize = new Vector2f(10, 100);
+            _shape = new RectangleShape(_playerOneSize);
+            _shape.Position = _PlayerOnePosition;
+        }
+
+        Vector2f _PlayerOnePosition;
+        Vector2f _playerOneSize;
+        RectangleShape _shape;
+
+
+        public Vector2f PlayerOnePosition
+        {
+            get { return _PlayerOnePosition; }
+            set { _PlayerOnePosition = value; }
+        }
+
+
+        public RectangleShape shape
+        {
+            get { return _shape; }
+            set { _shape = value; }
+        }
+
+        public Vector2f PlayerOneSize
+        {
+            get
+            {
+                return _playerOneSize;
+            }
+
+            set
+            {
+                _playerOneSize = value;
+            }
+        }
+    }
+}
