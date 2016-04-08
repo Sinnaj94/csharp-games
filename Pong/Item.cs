5e74cadcfa7d69b9bafd4099f86cef3056e4a1b7
Sinnaj94 <jannis.jahr@hotmail.de>
2016-04-08 21:59:11 +0200

    added item ( just class)

---
+++ Pong/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Graphics;
+
+namespace Pong
+{
+    class Item 
+    {
+        Vector2f position;
+        float size;
+        float sizeChange;
+        public Item()
+        {
+            position = getRandomPosition();
+            size = 20;
+            float sizeChange = 10;
+        }
+
+        Vector2f getRandomPosition()
+        {
+            Vector2f value;
+            Random r = new Random();
+            value.X = 400-size/2;
+            value.Y = r.Next(0, (int)(400-size/2));
+            return value;
+        }
+    }
+}
