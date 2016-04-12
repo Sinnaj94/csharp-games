2d7530664629e8e856f19a87481bb825ae64b482
Sinnaj94 <jannis.jahr@hotmail.de>
2016-04-12 22:03:28 +0200

    added developer text ;)

--- Pong/Intro.cs
+++ Pong/Intro.cs
@@ -13,14 +13,18 @@ namespace Pong
 
         Text introText;
         Text introTextBack;
+        Text developers;
         private int screenMid = 683;
 
         public Intro()
         {
             introText = new Text("Super Pong", ManageText.Instance.CrackmanFront, 400);
             introTextBack = new Text("Super Pong", ManageText.Instance.CrackmanFront, 410);
+            developers = new Text("Developed_by_Niklas_Hinte_and_Jannis_Jahr_HS_Bremen", ManageText.Instance.ArcadeClassic, 30);
+
             introText.Color = ManageText.Instance.Yellow;
             introTextBack.Color = ManageText.Instance.Grey;
+
         }
 
         public int updateIntro()
@@ -28,6 +32,7 @@ namespace Pong
             if(introText.CharacterSize > 20)
             {
                 introText.Position = new Vector2f(screenMid - introText.GetLocalBounds().Width / 2, 100);
+                developers.Position = new Vector2f(screenMid - developers.GetLocalBounds().Width / 2, 700);
                 introTextBack.Position = introText.Position;
                 introText.CharacterSize -= 2;
                 introTextBack.CharacterSize -= 2;
@@ -41,6 +46,7 @@ namespace Pong
         {
                 introText.Draw(target, states);
                 introTextBack.Draw(target, states);
+                developers.Draw(target, states);
         }
     }
 }
