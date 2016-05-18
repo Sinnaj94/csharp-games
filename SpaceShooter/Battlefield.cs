f18075cacbbf2ed0811febcbb8f2170b150315ba
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-05-18 19:43:25 +0200

    stuff was done

---
+++ SpaceShooter/Battlefield.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShooter
+{
+    // "Level" shall be loaded from file in the future
+    class Battlefield
+    {
+        int x = 10000;
+        int y = 10000;
+        public Battlefield()
+        {
+
+        }
+
+    }
+}
