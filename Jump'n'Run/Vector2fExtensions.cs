25eb3f7c800aa3fee3b6aaa77314e595e27f6933
Niklas <nhinte@stud.hs-bremen.de>
2016-06-10 10:26:07 +0200

    added

---
+++ Jump'n'Run/Vector2fExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FarseerPhysics;
+using Microsoft.Xna.Framework;
+
+namespace SpaceShooter
+{
+    public static class Vector2fExtensions
+    {
+        public static SFML.System.Vector2f ToSf(this Vector2 v) { return new SFML.System.Vector2f(ConvertUnits.ToDisplayUnits(v.X), ConvertUnits.ToDisplayUnits(v.Y)); }
+    }
+}
