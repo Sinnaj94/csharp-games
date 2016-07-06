23e2401af0d2e4cfe2ea39f8eb6ff9d34b855315
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-07-06 15:09:40 +0200

    Finally found an efficient way to convert collidable tiles into a convex poly

--- Jump'n'Run/TileMapBuilder.cs
+++ Jump'n'Run/TileMapBuilder.cs
@@ -27,44 +27,31 @@ namespace JumpAndRun
                 data[i / 4] = BitConverter.ToUInt32(bytes, i);
             }
             // Get outline polygon from pixels
-            Vertices verts = PolygonTools.CreatePolygon(data, (int)texture.Size.X, false);
-            Vector2 scale = ConvertUnits.ToSimUnits(new Vector2(1, 1));
-            verts.Scale(scale);
-            verts.Translate(- texture.Size.ToVector2f().ToSimVector() / 2);
-            verts = SimplifyTools.DouglasPeuckerSimplify(verts, 0.05f);
-            Body body = new Body(world);
-            List<Fixture> fixtures = FixtureFactory.AttachCompoundPolygon(
-                FarseerPhysics.Common.Decomposition.Triangulate.ConvexPartition(verts, TriangulationAlgorithm.Flipcode, false, 0.1f),
-                1, body);
-        }
+           // Vertices verts = PolygonTools.CreatePolygon(data, (int)texture.Size.X, false);
 
-        // Triangulate.ConvexPartition(verts, TriangulationAlgorithm.SeidelTrapezoids
+            Byte myByte = 1;
+            List<Vertices> _list = PolygonTools.CreatePolygon(data, (int)texture.Size.X, 0.05f, myByte, true, true);
+            Vertices verts = new Vertices();
+            Vector2 scale = ConvertUnits.ToSimUnits(new Vector2(2, 2));
 
-
-        static private void CreateCollisionWorld(World world, SFML.Graphics.Texture tilemap)
-        {
-            uint[] data = new uint[tilemap.Size.X * tilemap.Size.Y];
-            FarseerPhysics.Common.TextureTools.TextureConverter.DetectVertices(data, (int)tilemap.Size.X, true);
-            Vertices verts = PolygonTools.CreatePolygon(data, (int)tilemap.Size.X, true);
-            Vector2 scale = new Vector2(0.07f, 0.07f);
-            verts.Scale(ref scale);
-            List<Vertices> _list;
-            _list = Triangulate.ConvexPartition(verts, TriangulationAlgorithm.Bayazit);
-            Body body = new Body(world);
-            body.BodyType = BodyType.Static;
-            List<Fixture> compund = FixtureFactory.AttachCompoundPolygon(_list, 1, body);
-            //compund[0].Body.BodyType = BodyType.Dynamic;
+            foreach (Vertices v in _list)
+            {  
+                v.Scale(scale);
+                v.Translate(ConvertUnits.ToSimUnits(new Vector2(-16, -16)));
+                Body body = new Body(world);
+                List<Fixture> fixtures = FixtureFactory.AttachCompoundPolygon(
+                    FarseerPhysics.Common.Decomposition.Triangulate.ConvexPartition(SimplifyTools.DouglasPeuckerSimplify(v, 0.05f), TriangulationAlgorithm.Bayazit, false, 0.05f),
+                    1, body);
+            }
         }
 
         public TileMapBuilder(FarseerPhysics.Dynamics.World world)
         {
-
-            //CreateShape(new SFML.Graphics.Texture(@"Resources\sprites\tileset.png"), world);
- 
-            SFML.Graphics.Texture tilemap = new SFML.Graphics.Texture(@"Resources\Tield_Datei2.png");
+            CreateShape(new SFML.Graphics.Texture(@"Resources\Tield_Datei.png"), world);
+            SFML.Graphics.Texture tilemap = new SFML.Graphics.Texture(@"Resources\sprites\tileset.png");
             TiledSharp.TmxMap test = new TiledSharp.TmxMap(@"Resources\Tield_Datei.tmx");
             var myTileset = test.Tilesets["tileset"];
-          //  CreateCollisionWorld(world, tilemap);
+           
             foreach (TiledSharp.TmxLayer l in test.Layers)
             {
                 foreach (TiledSharp.TmxLayerTile t in l.Tiles)
@@ -73,21 +60,14 @@ namespace JumpAndRun
                     {
                         Body bodyToAdd = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(32), ConvertUnits.ToSimUnits(32), 10);
                         bodyToAdd.Position = new Microsoft.Xna.Framework.Vector2(ConvertUnits.ToSimUnits(t.X * 32), ConvertUnits.ToSimUnits(t.Y * 32));
-                        SFML.Graphics.Sprite bodySprite = new SFML.Graphics.Sprite(tilemap, new SFML.Graphics.IntRect((t.Gid % 30 - 1) * 32, t.Gid / 30 * 32, 32, 32));
-                        
+                        SFML.Graphics.Sprite bodySprite = new SFML.Graphics.Sprite(tilemap, new SFML.Graphics.IntRect((t.Gid % 30 - 1) * 32, t.Gid / 30 * 32, 32, 32));     
                         bodyToAdd.BodyType = BodyType.Static;
                         bodySprite.Position = new SFML.System.Vector2f(t.X * 32 - 16, t.Y * 32 - 16);
                         bodyToAdd.UserData = bodySprite;
-                        
-                        if (l.Name != "collidable")
-                        {
-                            bodyToAdd.Enabled = false;
-                        } 
-                        
-
+                        bodyToAdd.Enabled = false;
                     }
                 }
-            }      
+            }
         }
     }
 }
