3a28cf1f3a1af4945fe7729de5ea13fef5fa3d81
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-07-06 13:40:28 +0200

    tilemap adjustments

--- Jump'n'Run/TileMapBuilder.cs
+++ Jump'n'Run/TileMapBuilder.cs
@@ -40,16 +40,32 @@ namespace JumpAndRun
 
         // Triangulate.ConvexPartition(verts, TriangulationAlgorithm.SeidelTrapezoids
 
+
+        static private void CreateCollisionWorld(World world, SFML.Graphics.Texture tilemap)
+        {
+            uint[] data = new uint[tilemap.Size.X * tilemap.Size.Y];
+            FarseerPhysics.Common.TextureTools.TextureConverter.DetectVertices(data, (int)tilemap.Size.X, true);
+            Vertices verts = PolygonTools.CreatePolygon(data, (int)tilemap.Size.X, true);
+            Vector2 scale = new Vector2(0.07f, 0.07f);
+            verts.Scale(ref scale);
+            List<Vertices> _list;
+            _list = Triangulate.ConvexPartition(verts, TriangulationAlgorithm.Bayazit);
+            Body body = new Body(world);
+            body.BodyType = BodyType.Static;
+            List<Fixture> compund = FixtureFactory.AttachCompoundPolygon(_list, 1, body);
+            //compund[0].Body.BodyType = BodyType.Dynamic;
+        }
+
         public TileMapBuilder(FarseerPhysics.Dynamics.World world)
         {
 
-            CreateShape(new SFML.Graphics.Texture(@"Resources\sprites\tileset.png"), world);
-
-            SFML.Graphics.Texture tilemap = new SFML.Graphics.Texture(@"Resources\Tield_Datei.png");
+            //CreateShape(new SFML.Graphics.Texture(@"Resources\sprites\tileset.png"), world);
+ 
+            SFML.Graphics.Texture tilemap = new SFML.Graphics.Texture(@"Resources\Tield_Datei2.png");
             TiledSharp.TmxMap test = new TiledSharp.TmxMap(@"Resources\Tield_Datei.tmx");
             var myTileset = test.Tilesets["tileset"];
-
-            foreach(TiledSharp.TmxLayer l in test.Layers)
+          //  CreateCollisionWorld(world, tilemap);
+            foreach (TiledSharp.TmxLayer l in test.Layers)
             {
                 foreach (TiledSharp.TmxLayerTile t in l.Tiles)
                 {
@@ -58,7 +74,7 @@ namespace JumpAndRun
                         Body bodyToAdd = BodyFactory.CreateRectangle(world, ConvertUnits.ToSimUnits(32), ConvertUnits.ToSimUnits(32), 10);
                         bodyToAdd.Position = new Microsoft.Xna.Framework.Vector2(ConvertUnits.ToSimUnits(t.X * 32), ConvertUnits.ToSimUnits(t.Y * 32));
                         SFML.Graphics.Sprite bodySprite = new SFML.Graphics.Sprite(tilemap, new SFML.Graphics.IntRect((t.Gid % 30 - 1) * 32, t.Gid / 30 * 32, 32, 32));
-                        /*
+                        
                         bodyToAdd.BodyType = BodyType.Static;
                         bodySprite.Position = new SFML.System.Vector2f(t.X * 32 - 16, t.Y * 32 - 16);
                         bodyToAdd.UserData = bodySprite;
@@ -67,7 +83,7 @@ namespace JumpAndRun
                         {
                             bodyToAdd.Enabled = false;
                         } 
-                        */
+                        
 
                     }
                 }
