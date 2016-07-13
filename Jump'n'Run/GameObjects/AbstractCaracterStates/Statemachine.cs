779c138d7a455aa282850f3084dbb61a6e5e5863
Sinnaj94 <jannis.jahr@hotmail.de>
2016-07-13 14:55:07 +0200

    attack

--- Jump'n'Run/GameObjects/AbstractCaracterStates/Statemachine.cs
+++ Jump'n'Run/GameObjects/AbstractCaracterStates/Statemachine.cs
@@ -23,6 +23,7 @@ namespace JumpAndRun
             idle = new Idle(caracter, _temp.AnimationList.GetAnimation("idle", caracterTexture));
             walk = new Walk(caracter, _temp.AnimationList.GetAnimation("walk", caracterTexture));
             attack = new Attack(caracter, _temp.AnimationList.GetAnimation("attack", caracterTexture));
+            attack.Animation.SetSpeed(4);
         }
         public Statemachine(AbstractCaracter caracter, String spriteJsonAttribute, Texture caracterTextur)
         {
