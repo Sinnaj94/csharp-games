9af542d3626e12c5e0a7ee72f017eff4b91d03a7
AbteilungsleiterDerLiebe <nhinte@stud.hs-bremen.de>
2016-07-13 14:25:24 +0200

    added projectile classes

---
+++ Jump'n'Run/GameObjects/Projectiles/AbstractProjectile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpAndRun
+{
+    public abstract class AbstractProjectile : GameObject
+    {
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Jump'n'Run/GameObjects/Projectiles/MeleeProjectile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpAndRun
+{
+    public class MeleeProjectile : AbstractProjectile
+    {
+        
+        public MeleeProjectile(AbstractCaracter caracter)
+        {
+            this.body = new Body
+        }
+
+    }
+}
