61ea5df8735ed953fa281793a08c776dbff8b5bf
Jannis Jahr <jjahr@stud.hs-bremen.de>
2016-07-14 10:35:45 +0200

    put the object in the tiled file

--- Jump'n'Run/GameObjects/Crate.cs
+++ Jump'n'Run/GameObjects/Crate.cs
@@ -1,4 +1,5 @@
 ﻿using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
 using SFML.Graphics;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,14 @@ namespace JumpAndRun
 {
     class Crate : AbstractPhysicsObject
     {
-        public Crate(Body b)
+        public Crate(Body b, Vector2 position)
         {
             Init(b);
             b.BodyType = BodyType.Dynamic;
             b.LinearDamping = 1.5f;
             b.AngularDamping = 1.5f;
             Sprite = new Sprite(new Texture(@"Resources\sprites\crate.png"));
+            b.Position = position;
         }
         public override void Update()
         {
